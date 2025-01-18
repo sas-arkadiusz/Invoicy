29636a0ded98727f920040cd2d683f291318b3fc
Arkadiusz Sas <kontakt.sas.arkadiusz@gmail.com>
2025-01-18 12:02:03 +0100

    Project initialization

---
+++ src/main/java/pl/arkadiuszsas/invoicy/InvoicyApplication.java
@@ -0,0 +1,13 @@
+package pl.arkadiuszsas.invoicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/arkadiuszsas/invoicy/InvoicyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arkadiuszsas.invoicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
